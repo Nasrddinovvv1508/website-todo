6757528cbdfc6b6b70db90bea5250b2b318e57d9
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-06-29 16:43:14 +0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='bg-base-300'>
+      <div className='align-elements'>
+        <nav className='navbar'>
+          <div className="navbar-start">
+            <Link to='/'>
+              <h1 className='text-4xl font-bold'>Logo</h1>
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <button className='btn btn-error text-white'>Log Out</button>
+          </div>
+        </nav>
+      </div>
+
+    </header>
   )
 }
 
